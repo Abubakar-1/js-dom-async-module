3bd27736782f17459c5a13b9796b1712f90ebb73
Abubakar-1 <abubakaruabdulkadir@gmail.com>
2025-08-07 16:42:34 +0100

    part 2

--- tasks/callback-assignment/fetchDataCallback.js
+++ tasks/callback-assignment/fetchDataCallback.js
@@ -1,11 +1,34 @@
+// Part: 1
+
+// function fetchDataWithCallback(callback) {
+//   setTimeout(() => {
+//     callback("Data fetched");
+//   }, 2000);
+//   // this simulates the delay for 2 secs, before thecallback is called.
+// }
+
+// fetchDataWithCallback(function (message) {
+//   console.log(message);
+//   // this is the usage of the function,that console.logs what is returned from the callback function
+// });
+
+// Part 2:
+
 function fetchDataWithCallback(callback) {
+  let error = true;
+
   setTimeout(() => {
-    callback("Data fetched");
+    try {
+      if (error) {
+        throw new Error("Fetch failed");
+      }
+      callback("Data fetched");
+    } catch (err) {
+      console.error("Error:", err.message);
+    }
   }, 2000);
-  // this simulates the delay for 2 secs, before thecallback is called.
 }
 
 fetchDataWithCallback(function (message) {
   console.log(message);
-  // this is the usage of the function,that console.logs what is returned from the callback function
 });
